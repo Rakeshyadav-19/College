e82553f7e1077f617431e4c6e6b56ad298c6c67f
rakesyadav19 <rakeshyadavry087@gmail.com>
2023-11-24 09:50:40 +0530

    Files name changed and new exp 6.1 and 6.2 added

---
+++ Data structure/Exp6.1QueueusingArray.c
@@ -0,0 +1,67 @@
+//To Implement Program for Stack using List
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define MAX_SIZE 10
+int queue[MAX_SIZE];
+int front = -1, rear = -1;
+void enqueue() {
+    int data;
+    if ((rear + 1) % MAX_SIZE == front) {
+        printf("Queue is full\n");
+        return;
+    }
+   printf("Enter Value To Enqueue: ");
+    scanf("%d", & data);
+    if (front == -1 && rear == -1) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % MAX_SIZE;
+    }
+    queue[rear] = data;
+}
+void dequeue() {
+    int value = queue[front];
+    if (front == -1 && rear == -1) {
+        printf("Queue is empty\n");
+        return;
+    }
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % MAX_SIZE;
+    }
+    printf("Dequeued Element: %d\n", value);
+}
+void display() {
+    int i;
+    if (front == -1)
+        printf("Queue is empty\n");
+    else {
+        printf("Queue is : ");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue[i]);
+        printf("\n");
+    }
+}
+int main() {
+    int ch;
+    printf("**Static Queue**\n\n");
+    printf("1. Enqueue\n2. Dequeue\n3. Display All Elements\n4. Exit");
+    do {
+        printf("\nEnter Your Choice: ");
+        scanf("%d", & ch);
+        if (ch == 1) {
+            enqueue();
+        } else if (ch == 2) {
+            dequeue();
+        } else if (ch == 3) {
+            display();
+        } else if (ch == 4) {
+            exit(0);
+        } else {
+            printf("Invalid Choice\n");
+        }
+    } while (ch != 4);
+    return 0;
+}
---
+++ Data structure/Exp6.2QueueusingList.c
@@ -0,0 +1,70 @@
+//To Implement Program for Queue using List
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node *next;
+};
+struct Node *front = NULL, *rear = NULL;
+void enqueue() { 
+    int data;
+    printf("Enter Value To Enqueue: ");
+    scanf("%d", & data);
+    struct Node * nptr = malloc(sizeof(struct Node)); 
+    nptr -> data = data;
+    nptr -> next = NULL;
+    if (rear == NULL) { 
+        front = nptr; 
+        rear = nptr;
+    } else {
+        rear -> next = nptr; 
+        rear = rear -> next;
+    }
+}
+void dequeue() { 
+    if (front == NULL) { 
+        printf("Queue is Empty\n");
+    } else {
+        struct Node * temp = front;
+        front = front -> next; 
+        if (front == NULL) {
+            rear = NULL; 
+        }
+        printf("Dequeued Element: %d\n", temp -> data); 
+        free(temp); 
+    }
+}
+void display() { 
+    struct Node * temp = front;
+    if (front == NULL) { 
+        printf("Queue is Empty\n");
+        return;
+    }
+    printf("Queue is: ");
+    while (temp != NULL) {
+        printf(" %d ", temp -> data); 
+        temp = temp -> next; 
+    }
+    printf("\n");
+}
+int main() {
+    int ch; 
+    printf("**Dynamic Queue**\n\n");
+    printf("1. Enqueue\n2. Dequeue\n3. Display All Elements\n4. Exit");
+    do {
+        printf("\nEnter Your Choice: ");
+        scanf("%d", &ch); 
+        if (ch == 1) { 
+            enqueue();
+        } else if (ch == 2) { 
+            dequeue();
+        } else if (ch == 3) { 
+            display();
+        } else if (ch == 4) { 
+            exit(0);
+        } else { 
+            printf("Invalid Choice\n");
+        }
+    } while(ch != 4); 
+    return 0;
+}
