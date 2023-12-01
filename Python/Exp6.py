d14e6da8b975556d8e77bd5d9f04e40bf50f4cde
rakesyadav19 <rakeshyadavry087@gmail.com>
2023-12-01 10:13:04 +0530

    Python exp 6

--- Python/Exp6.py
+++ Python/Exp6.py
@@ -1,6 +1,6 @@
-#Q. Perform 5 manipulations operations open,close,read,write,append and copy from one file to another.
-import sys
+#Perform 5 manipulations operations open,close,read,write,append and copy from one file to another.
 
+import sys
 randomlist= ['a',0,2]
 for entry in randomlist:
     try:
@@ -10,5 +10,30 @@ for entry in randomlist:
     except:
         print("oops!",sys.exc_info()[0],"occured")
         print("Next entry")
-        print()
 print("The reciprocal of entry  is",r)
+print("\n")
+
+#2
+import sys
+randomlist = ['a',0,2]
+for entry in randomlist:
+    try:
+        print("The entry is ",entry)
+        r = 1/int(entry)
+        break
+    except:
+        print("oops!",sys.exc_info()[0],"occured")
+        print("Next entry")
+        print("\n")
+
+#3
+try:
+    pass
+except ValueError:
+     pass
+except ZeroDivisionError:
+    print("Type error ZeroDivisionError has occured")
+except :
+    pass
+
+#Console
